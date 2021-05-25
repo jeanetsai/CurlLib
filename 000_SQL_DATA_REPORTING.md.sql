df27b8b92f60ba6c3807d1ab22cc57dfecc7c536
jeanetsai <52280399+jeanetsai@users.noreply.github.com>
2021-05-25 15:03:41 +0800

    Add files via upload

--- 000_SQL_DATA_REPORTING.md.sql
+++ 000_SQL_DATA_REPORTING.md.sql
@@ -211,79 +211,166 @@ ORDER BY ct DESC
 <GROUP BY的欄位要被SELECT避免出錯>
 
 02_02_having
+SELECT district, count(district) as ct
+FROM address 
+GROUP BY district
+HAVING count(district)>8 --HAVING ct>8
+ORDER BY ct DESC
 
+<MIN/MAX/SUM/AVG>
+SELECT sum(rental_duration)
+From table
 
-
+SELECT rating, avg(rental_duration)
+From table
+GROUP BY rating
 
 02_03_distinct
+SELECT DISTINCT(district)
+FROM address
 
+SELECT DISTINCT CONCAT(first_name, last_name)
+FROM actor
 
+SELECT DISTINCT YEAR(last_update)
+FROM actor
 
+<COUNT+DISTINCT>
+
+SELECT COUNT(DISTINCT YEAR(last_update))
+FROM actor
 
 02_04_grpConcat
+SELECT distinct district, group_concat(phone)
+FROM address
+GROUP BY district
 
+SELECT distinct district, group_concat(phone)
+FROM address
+GROUP BY district
 
+SELECT distinct district, group_concat(phone ORDER BY phone ASC SEPARATOR ';')
+FROM address
+GROUP BY district
 
+--我們如果要在 GROUP BY 中把字串「黏起來」，不是使用 CONCAT()，而是使用另一個函式：GROUP_CONCAT()。--
 
 03_01_limit
+SELECT * 
+FROM table
+LIMIT 25
 
-
-
+--優點:節省時間
 
 03_02_keys
 
+primary key
+unique, auto_increment
 
-
-
+foreign key
 
 03_03_visualisation
 
-
-
-
+A 的 foreign key 
+可以= 
+B 的 primary key
+DB設計
+常用的為主表格 
+不常用的為其他表格
 
 03_04_join
 
-
-
+SELECT customer.first_name
+FROM customer
+JOIN address
+ON customer.id = address.id
+WHERE district = 'abc'
+--JOIN ON 要在一起
 
 03_05_moreJoins
+JOIN 就是 INNER JOIN
 
+<我要左邊全部+交集>
+SELECT *
+FROM tableA
+LEFT JOIN tableB
+on tableA.id = tableB.id
+回傳的tableB可能包含NULL
 
-
+<我要左邊扣掉重複的>
+SELECT *
+FROM tableA
+LEFT JOIN tableB
+on tableA.id = tableB.id
+WHERE tableB.id IS NULL
 
 03_06_union
+JOIN水平整合兩個TABLE
+UNION垂直整合兩個TABLE
+
+SELECT date(last_update) FROM actor
+UNION
+SELECT date(last_update) FROM address
 
 
+SELECT date(last_update) FROM actor
+UNION ALL
+SELECT date(last_update) FROM address
 
+
+UNION ALL 和 UNION 不同 之處在於 UNION ALL 會將每一筆符合條件的資料都列出來，無論資料值 有無重複。
+UNION傳回的是DISTINCT的值
 
 03_07_usingIn
+<IN>
+SELECT
+WHERE + IN
 
+SELECT * FROM actor
+WHERE name IN ('aa','bb','cc')
 
+SELECT * FROM rental
+WHERE id IN (
+SELECT id FROM customer 
+WHERE first_name='Jennifer'
+)
 
 
 03_08_subquery
 
+SELECT FROM
+(SELECT FROM) AS
 
+SELECT f.first_name FROM
+(SELECT first_name FROM actor) AS f
 
+SELECT f.fn FROM
+(SELECT first_name as fn FROM actor) AS f
+
+--別名可以放在前面--
 
 04_01_views
+views: a stored query
 
-
+SHOW CREATE VIEW table_actor
 
 
 04_02_function
 
-
+--可取代 subquery / nested select
 
 
 04_03_proc
+stored procedure改數值
+function不改數值只傳回value
+可以在stored procedure裡面
+放function反之則不可
 
-
-
-
+CALL film_not_in_stock(2,2,@count);
+--FUNCTION(參數)
+SELECT * FROM blah WHERE field = @count 
 
 04_04
-
+done
 
 Source: (L) SQL for Data Reporting and Analysis
